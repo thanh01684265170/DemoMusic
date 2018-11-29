d167f97ae3c4bbd1155148a315fa19b9ba29144e
thanh <thanh01684265170@gmail.com>
2018-11-29 09:11:03 +0700

    create app

---
+++ app/src/main/java/com/example/anh/mp3play/screen/home/ViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.anh.mp3play.screen.home;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.anh.mp3play.screen.home.album.AlbumFragment;
+import com.example.anh.mp3play.screen.home.song.SongFragment;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    ViewPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        Fragment frag=null;
+        switch (position){
+            case 0:
+                frag = new SongFragment();
+                break;
+            case 1:
+                frag = new AlbumFragment();
+                break;
+        }
+        return frag;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position){
+            case 0:
+                title = "One";
+                break;
+            case 1:
+                title = "Two";
+                break;
+        }
+        return title;
+    }
+}
